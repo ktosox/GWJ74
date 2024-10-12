0187ada76a88bc41264b451995c7149116cea754
Ktosox <ktoso3@gmail.com>
2024-10-12 22:16:00 +0200

    started work on a high score leaderboard
    and added some fonts

---
+++ Game/experimental/keyboard.gd
@@ -0,0 +1,17 @@
+extends ColorRect
+
+signal key_selected(key : String)
+
+signal key_deleted
+
+signal name_confirmed
+
+# WHAT THIS DOES
+
+# shows keyboard
+
+# allows selection of characters by directional keys
+
+# has interface for sending selected keys
+
+# has interface for sending delte and confirm calls
