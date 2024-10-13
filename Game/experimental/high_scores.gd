166da6b52b1b33f78bd940a653dc580b9da8a24e
Ktosox <ktoso3@gmail.com>
2024-10-13 13:09:24 +0200

    had to do a minor re-work
    grid container arranges nodes by rows, and I need to arrange scores by columns, which makes them very incompatible

--- Game/experimental/high_scores.gd
+++ Game/experimental/high_scores.gd
@@ -4,21 +4,54 @@ var score_scene = preload("res://experimental/score.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for test in $Layout.get_children():
-		if randi()%2:
-			test.set_label("")
-			test.toggle()
-	insert_new_score(4).set_label("HELLO")
 
-func update_order() -> void:
-	
+	var new_score = insert_new_score(1)
+	new_score.set_label("HELLO")
+	new_score.set_score(50)
+	new_score = insert_new_score(1)
+	new_score.set_label("DERP")
+	new_score.set_score(10)
+	new_score = insert_new_score(8)
+	new_score.set_label("LOSE")
+	new_score.set_score(1)
+	new_score = insert_new_score(7)
+	new_score.set_label("WIN")
+	new_score.set_score(10)
+
+#func update_order() -> void: # hahhaHAHahAHAHahhAHAHAha
+	#var counter = 0.0
+	#for score in $Layout.get_children():
+		#counter += 0.5
+		#var score_value = roundi(counter) 
+		#if roundi(counter) == int(counter):
+			#score_value +=5
+		#score.set_index_number(score_value)
+	#pass
+
+func update_order():
+	var index_number = 1
+	for left_score in $LayoutLeft.get_children():
+		left_score.set_index_number(index_number)
+		index_number += 1
+	for right_score in $LayoutRight.get_children():
+		right_score.set_index_number(index_number)
+		index_number += 1
 	pass
+	
 
 
 func insert_new_score(index : int) -> Node:
 	var new_score = score_scene.instantiate()
-	$Layout.add_child(new_score)
-	$Layout.move_child(new_score,index)
-	$Layout.get_child(10).queue_free()
+	if index < 5:
+		$LayoutLeft.add_child(new_score)
+		$LayoutLeft.move_child(new_score,index-1)
+		var score_to_reparent = $LayoutLeft.get_child(4)
+		score_to_reparent.reparent($LayoutRight)
+		$LayoutRight.move_child(score_to_reparent,0)
+	else:
+		$LayoutRight.add_child(new_score)
+		$LayoutRight.move_child(new_score,index-5)
+	$LayoutRight.get_child(4).queue_free()
+	update_order()
 	return new_score
 	pass
--- Game/experimental/score.gd
+++ Game/experimental/score.gd
@@ -1,13 +1,18 @@
 extends HBoxContainer
 
+
 func toggle() -> void:
 	if $BlinkyLine/Blinker.is_playing():
 		$BlinkyLine/Blinker.play("RESET")
 	else:
 		$BlinkyLine/Blinker.play("blink")
 
-func set_number(number : int) -> void:
+func set_index_number(number : int) -> void:
 	$Number.text = str(number) + '.'
+
+func set_score(score : int) -> void:
+	$Score.text = str(score) +'-'
+	pass
 
 func set_label(text : String) -> void:
 	$StoredName.text = text
